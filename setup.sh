ed325240e3b13f1a988992e0a087eac0cf099076
heisson.lima <heisson.lima@celus.io>
2023-09-09 08:39:01 +0200

    feat: setxkbmap and todos

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# TODO:
+# - Map function keys: https://github.com/jergusg/k380-function-keys-conf
+# - Invert mouse buttons
+# - Invert trackpad directions
+
+# Upload newly created branch with "git push"
+git config --global --add --bool push.autoSetupRemote true
+
+# Setup keyboard
+setxkbmap -layout us -variant intl
