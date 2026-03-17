6f5ce51c07a5af1d4f7761edc1cb3c757dcd27e8
Heisson Willen <heisson70@gmail.com>
2026-03-17 19:40:19 +0100

    chore: end of the Linux era I guess

--- setup.sh
+++ setup.sh
@@ -1,20 +1,13 @@
 # TODO:
 # - Map function keys: https://github.com/jergusg/k380-function-keys-conf
-# - Shortcut to change from light to dark theme
-# - Install Chrome
 
 function create_symlinks() {
     dotfiledir="${HOME}/dotfiles"
     files=(
-        .config/i3
-        .config/i3status
-        .screenlayout
-        Projects/Personal/.gitconfig
         .aliases
         .gitconfig
         .gitignore
         .vimrc
-        .Xmodmap
         .zshrc
     )
 
@@ -28,27 +21,5 @@ function create_symlinks() {
     done
 }
 
-function install_packages() {
-    echo "Updating APT and installing software packages."
-    sudo apt update
-    sudo apt install -y xinput i3 pip arandr zsh curl wget brightnessctl terminator neofetch blueman alsa-utils pulseaudio
-
-    echo "Installing non-APT packages"
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp    
-    sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
-}
-
-function setup_misc() {
-    echo "Setting up keyboard layout."
-    setxkbmap -layout us -variant intl
-
-    echo "Adding user to video group. This allows sudoless 'brightnessctl' usage."
-    sudo usermod -aG video ${USER}
-
-    echo "Remap keys"
-    xmodmap ~/.Xmodmap
-}
-
-install_packages
-setup_misc
 create_symlinks
+
